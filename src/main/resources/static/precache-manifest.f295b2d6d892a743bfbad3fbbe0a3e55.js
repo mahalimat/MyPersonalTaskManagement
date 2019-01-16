5b32447651a52002173feb33476ba05768c71e7b
Mathan <mathanm@gmail.com>
2019-01-16 18:14:23 -0500

    react

---
+++ src/main/resources/static/precache-manifest.f295b2d6d892a743bfbad3fbbe0a3e55.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a97e16bdf1b8f22bc48f",
+    "url": "/static/css/main.edab9828.chunk.css"
+  },
+  {
+    "revision": "a97e16bdf1b8f22bc48f",
+    "url": "/static/js/main.a97e16bd.chunk.js"
+  },
+  {
+    "revision": "edfb13452e7eb37b0fb7",
+    "url": "/static/css/1.4522581e.chunk.css"
+  },
+  {
+    "revision": "edfb13452e7eb37b0fb7",
+    "url": "/static/js/1.edfb1345.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "022d81937010929e52a810c111382ac4",
+    "url": "/index.html"
+  }
+];
